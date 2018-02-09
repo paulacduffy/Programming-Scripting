aaf7decab5958257ae90233ebd72037128d9cc17
paulacduffy <G00352482@gmit.ie>
2018-02-09 20:14:46 +0000

    Create Collatz.py

---
+++ Collatz.py
@@ -0,0 +1,14 @@
+#Paula Duffy 2018-02-06
+#Collatz Conjecture
+
+n = int(input("Please enter an integer: "))
+
+n = 17
+while n > 1:
+  if n % 2 == 0:
+    n = n // 2
+    print (n)  
+  
+  elif n % 2 == 1:
+    n = (n * 3) + 1
+    print (n)
