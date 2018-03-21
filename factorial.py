de823287b6b88bc79221e8cfff9c3b647a44eca1
paulacduffy <G00352482@gmit.ie>
2018-03-21 20:14:14 +0000

    Added comment to explain code

--- factorial.py
+++ factorial.py
@@ -4,8 +4,8 @@
 def factorial(x):
     n = 1
     while x >= 1:
-        n = n * x
-        x = x - 1
+        n = n * x 
+        x = x - 1 #reduce x by 1 until x = 1, loop multiplies answers together to get factorial
     return n
 
 print ("The factorial of 7 is:", factorial(7))
