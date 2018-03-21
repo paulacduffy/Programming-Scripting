4116a0fadf16cde1071e064ffeaf0a7288ab73dd
paulacduffy <G00352482@gmit.ie>
2018-03-21 19:59:52 +0000

    Added comments

--- Collatz.py
+++ Collatz.py
@@ -3,12 +3,11 @@
 
 n = int(input("Please enter an integer: "))
 
-n = 17
 while n > 1:
   if n % 2 == 0:
-    n = n // 2
+    n = n // 2 #divide by 2 if integer is even
     print (n)  
   
   elif n % 2 == 1:
-    n = (n * 3) + 1
+    n = (n * 3) + 1 #multiply by 3 and add 1 if integer is not even
     print (n)
